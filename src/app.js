1649706798842521b34ad95ca677a0b19f8160f9
cxycool <sodaforever@outlook.com>
2022-06-15 22:22:09 +0800

    update

--- src/app.js
+++ src/app.js
@@ -101,7 +101,7 @@ const app = new Vue({
             login: post.user.login,
             created_at: post.created_at,
             title: post.title,
-            body: marked.parse(post.body), //解析markdown
+            body: marked.parse(post.body || post.title || "无题"), //解析markdown
             isshowpic: false
           })
         }
