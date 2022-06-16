0512323522a59971084c14007a1edf15b47859cc
soda <107160391+cxycool@users.noreply.github.com>
2022-06-16 17:54:53 +0800

    Update config.js

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@ var _config = {
   blog_url: "", //博客地址
   blog_pub_url: "", //博客发布地址
   owner: "cxycool", // github 用户名
-  nickname: "soda", //用户昵称
+  nickname: "什么名字呢", //用户昵称
   repo: "moment", // github 中对应仓库名
   blog_author_type: "OWNER", //发布博客的用户 默认 OWNER
   access_token: "ghp_vHI3gitgac7JDCkNH" + "68rTxdMDzCTxe0rWqSU", // 请求量大时需要在 github 后台单独设置一个读取公开库的 token, 注意将token 拆成两个字符串，否则会被系统自动删除掉
